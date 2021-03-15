31b5d5b2f7eac206637c80e74be357800c33c3a5
Ryan Avery <ravery@ucsb.edu>
2021-03-15 11:02:05 -0700

    use length of tseries for loop

--- examples/save_tifs.R
+++ examples/save_tifs.R
@@ -1,14 +1,14 @@
 data(s1vv_lndvi_raster)
 layer_dates = format(as.Date(substr(names(lndvi),10,16), format="%Y%j"))
 layer_filenames = paste(layer_dates, "-lndvi.tif", sep="")
-for(i in 1:291) {
+for(i in 1:length(layer_filenames)) {
   single_band <- raster(lndvi, layer = i)
   writeRaster(single_band, layer_filenames[i], overwrite=TRUE)
 }
 
 layer_dates = format(as.Date(substr(names(s1vv),2,11), format="%Y.%m.%d"))
 layer_filenames = paste(layer_dates, "-s1vv.tif", sep="")
-for(i in 1:291) {
+for(i in 1:length(layer_filenames)) {
   single_band <- raster(s1vv, layer = i)
   writeRaster(single_band, layer_filenames[i], overwrite=TRUE)
 }
